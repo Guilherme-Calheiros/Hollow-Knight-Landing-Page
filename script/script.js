05b9f430dbea6a3da8432a4a0fa33320809141e2
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-09-28 11:51:21 -0300

    feat: adding script to change images

--- script/script.js
+++ script/script.js
@@ -7,3 +7,22 @@ arrowBtns.forEach(btn => {
         carousel.scrollLeft += btn.id === "left_arrow" ? -firstCardWidth : firstCardWidth;
     })
 });
+
+const imagens = [
+    "../images/ferrao.png",
+    "../images/mascara.jpg",
+    "../images/knight-jump.png"
+];
+let imageElement = document.getElementById("homeImage")
+let index = 0;
+
+function alterarImagens(){
+    imageElement.style.opacity = 0;
+    setTimeout(() => {
+        imageElement.src = imagens[index];
+        index = (index + 1) % imagens.length;
+        imageElement.style.opacity = 1;
+    }, 1000)
+}
+
+setInterval(alterarImagens, 5000);
